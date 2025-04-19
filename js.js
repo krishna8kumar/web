3866b2e229d1be4d79df516f1708f61c3b622bef
krishna8kumar <136232219+krishna8kumar@users.noreply.github.com>
2025-04-19 09:38:53 +0530

    edited

---
+++ js.js
@@ -0,0 +1,24 @@
+// function one(names){
+//     console.log(`this is me ${names}`)
+// }
+// function two(oneFn){
+//     const names="krishna"
+//     oneFn(names)
+// }   
+// two(one)
+// const os =require("os")
+                            // *** writing and reading a file in node js sync...***//
+                            
+// const fs= require("fs")
+// fs.writeFileSync("text.txt","hii")
+
+// const fs= require("fs")
+// const data=fs.readFileSync("text.txt","utf-8")
+// console.log(data)
+
+                         // *** writing and reading a file in node js Async...***//
+// const fs=require("fs")
+// fs.readFile("text.txt","utf-8",(err,data)=>{
+//     if (err) throw err;
+//     else console.log(data)
+// })
